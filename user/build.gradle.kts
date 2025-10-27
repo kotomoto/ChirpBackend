5c1d5d107f8a29031dc400fb4f88814a7493ac42
jkotor <jkotor@gmail.com>
2025-10-27 22:38:57 +0100

    Add build.gradle for user module

---
+++ user/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+}
+
+group = "com.koto"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
