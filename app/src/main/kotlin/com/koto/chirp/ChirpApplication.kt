091d64e71f66c568375427ec300acab6f4dd023d
jkotor <jkotor@gmail.com>
2026-01-12 22:57:06 +0100

    Add commented demo code

--- app/src/main/kotlin/com/koto/chirp/ChirpApplication.kt
+++ app/src/main/kotlin/com/koto/chirp/ChirpApplication.kt
@@ -1,8 +1,12 @@
 package com.koto.chirp
 
 import Test
+import com.koto.chirp.infra.database.entities.UserEntity
+import com.koto.chirp.infra.database.repositories.UserRepository
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class ChirpApplication
@@ -11,3 +15,17 @@ fun main(args: Array<String>) {
     Test()
 	runApplication<ChirpApplication>(*args)
 }
+
+//@Component
+//class Demo(
+//    private val userRepo: UserRepository,
+//) {
+//    @PostConstruct
+//    fun init() {
+//        userRepo.save(UserEntity(
+//            email = "test@test.com",
+//            username = "test",
+//            hashedPassword = "123"
+//        ))
+//    }
+//}
